42e74acf6a1ad4a6ec33f0eca57fa1143dc1970b
Tristan <darkskiiittles@gmail.com>
2024-06-14 11:03:14 -0400

    aidan project cms initial commit

---
+++ versions/5.85.0/content/themes/aidan-ghost-theme/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.js', './*.hbs'], // Specify the input files here
+  darkMode: media, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
