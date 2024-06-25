311a2c4d0e92f4ac3f58f2c1bbd49d9b806fb7ae
Tristan <darkskiiittles@gmail.com>
2024-06-25 00:13:32 -0400

    theme commit

--- versions/5.85.0/content/themes/aidan-ghost-theme/tailwind.config.js
+++ versions/5.85.0/content/themes/aidan-ghost-theme/tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.js', './*.hbs'], // Specify the input files here
-  darkMode: media, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ['./src/**/*.html', './src/**/*.js', './*.hbs'], // Specify the input files here
+    darkMode: media, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FF6347',
+                secondary: '#4682B4',
+                accent: '#FFD700',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
